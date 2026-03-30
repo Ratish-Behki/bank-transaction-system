1d303487d53ab1f1e90ff8edb0db88444361a884
Ratish <ratishb.me.23@nitj.ac.in>
2026-03-30 23:48:07 +0530

    final cors fix

--- backend-ledger/src/app.js
+++ backend-ledger/src/app.js
@@ -6,7 +6,11 @@ const cors = require("cors")
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://bank-transaction-frontend.onrender.com"
+  ],
+  methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
 
